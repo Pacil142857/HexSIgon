73f309497937c01aa8e7daef0a4cfd44e8912f67
Pacil142857 <austinmcgregor2@gmail.com>
2023-10-14 11:49:54 -0400

    Encapsulate Hex

---
+++ Hex.py
@@ -0,0 +1,44 @@
+class Hex:
+    def init(self, meter=0, kilogram=0, second=0, ampere=0, kelvin=0, mole=0, candela=0):
+        self.meter = meter
+        self.kilogram = kilogram
+        self.second = second
+        self.ampere = ampere
+        self.kelvin = kelvin
+        self.mole = mole
+        self.candela = candela
+    
+    # Add methods
+    def addMeter(self):
+        self.meter += 1
+    def addKilogram(self):
+        self.kilogram += 1
+    def addSecond(self):
+        self.second += 1
+    def addAmpere(self):
+        self.ampere += 1
+    def addKelvin(self):
+        self.kelvin += 1
+    def addMole(self):
+        self.mole += 1
+    def addCandela(self):
+        self.candela += 1
+    
+    # Remove methods
+    def removeMeter(self):
+        self.meter -= 1
+    def removeKilogram(self):
+        self.kilogram -= 1
+    def removeSecond(self):
+        self.second -= 1
+    def removeAmpere(self):
+        self.ampere -= 1
+    def removeKelvin(self):
+        self.kelvin -= 1
+    def removeMole(self):
+        self.mole -= 1
+    def removeCandela(self):
+        self.candela -= 1
+
+    def __str__(self):
+        return f"Meter: {self.meter}, Kilogram: {self.kilogram}, Second: {self.second}, Ampere: {self.ampere}, Kelvin: {self.kelvin}, Mole: {self.mole}, Candela: {self.candela}"
