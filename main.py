4a836c9628dc2210d492f7b0fe59ec727b5385bd
Pacil142857 <austinmcgregor2@gmail.com>
2023-10-14 16:47:46 -0400

    Add working demo

--- main.py
+++ main.py
@@ -6,24 +6,26 @@ from OrderUI import OrderUI
 # Initalize pygame with a screen
 pygame.init()
 width, height = pygame.display.Info().current_w, pygame.display.Info().current_h
-screen = pygame.display.set_mode(width, height)
+screen = pygame.display.set_mode((width, height))
 
 board = Board()
 hexes = []
 orders = []
-radius = height / 5
+radius = height / 6
 center_x = 2 * width // 3
 center_y = height // 2
 
 # Add HexUIs
-hexes.add(HexUI(board.hexes[0], center_x - 5 * radius // 3, center_y - radius, radius))
-hexes.add(HexUI(board.hexes[1], center_x - 5 * radius // 3, center_y + radius, radius))
-hexes.add(HexUI(board.hexes[2], center_x, center_y - 2 * radius, radius))
-hexes.add(HexUI(board.hexes[3]), center_x, center_y, radius)
-hexes.add(HexUI(board.hexes[2], center_x, center_y + 2 * radius, radius))
-hexes.add(HexUI(board.hexes[5], center_x + 5 * radius // 3, center_y - radius, radius))
-hexes.add(HexUI(board.hexes[6], center_x + 5 * radius // 3, center_y + radius, radius))
+hexes.append(HexUI(board.hexes[0], center_x - 5 * radius // 3, center_y - radius, radius))
+hexes.append(HexUI(board.hexes[1], center_x - 5 * radius // 3, center_y + radius, radius))
+hexes.append(HexUI(board.hexes[2], center_x, center_y - 2 * radius, radius))
+hexes.append(HexUI(board.hexes[3], center_x, center_y, radius))
+hexes.append(HexUI(board.hexes[2], center_x, center_y + 2 * radius, radius))
+hexes.append(HexUI(board.hexes[5], center_x + 5 * radius // 3, center_y - radius, radius))
+hexes.append(HexUI(board.hexes[6], center_x + 5 * radius // 3, center_y + radius, radius))
 
+# Add orders
+# orders.append(OrderUI(board.orders[0], ))
 
 run = True
 while run:
@@ -32,6 +34,9 @@ while run:
             run = False
     
     screen.fill((0, 0, 0))
+    for hex in hexes:
+        hex.draw(screen)
+    pygame.display.flip()
     
 pygame.quit()
 exit()
