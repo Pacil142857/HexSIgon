819e10dbc2048ff8e9fd27bb79f79eed6493c118
Pacil142857 <austinmcgregor2@gmail.com>
2023-10-14 14:25:48 -0400

    Add class to hold UI stuff for orders

---
+++ OrderUI.py
@@ -0,0 +1,21 @@
+import pygame
+
+class OrderUI:
+    def __init__(self, hex, x, y, rect_color=(255, 255, 255), text_color=(255, 255, 255)):
+        self.hex = hex
+        self.x = x
+        self.y = y
+        self.rect_color = rect_color
+        self.text_color = text_color
+    
+    def draw(self, surface):
+        # Render the text
+        font = pygame.font.SysFont("Courier New", 40)
+        text = font.render(str(hex), True, self.text_color)
+        rect = text.get_rect(center = (self.x, self.y))
+        
+        # Draw the rectangle
+        pygame.draw.rect(surface, self.rect_color, rect, 1, 4)
+        
+        # Draw the text in the rectangle
+        surface.blit(text, rect)
