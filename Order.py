07a2d979ce91609226366470bd4b3c53bb9a7d88
Pacil142857 <austinmcgregor2@gmail.com>
2023-10-14 13:48:16 -0400

    Implement methods

--- Order.py
+++ Order.py
@@ -4,3 +4,11 @@ class Order:
         self.points = points
         self.quantity_name = quantity_name
         self.unit_name = unit_name
+    
+    def compare_to(self, hex):
+        return self.hex == hex
+    
+    def __str__(self):
+        if (self.unit_name):
+            return f"Create a {self.unit_name}, the unit for {self.quantity_name}"
+        return f"Create the unit for {self.quantity_name}"
