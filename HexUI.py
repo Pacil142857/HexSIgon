35c2f990034ce62bec8eca0fe766a96230e49a38
Pacil142857 <austinmcgregor2@gmail.com>
2023-10-14 18:33:05 -0400

    Add fill color

--- HexUI.py
+++ HexUI.py
@@ -3,8 +3,8 @@ from math import sin
 from math import tau
 import pygame
 
-def draw_hex(x, y, radius, color=(255, 255, 255)):
-    return [color, [(x + radius * cos(tau * i / 6), y + radius * sin(tau * i / 6)) for i in range(6)], 1]
+def draw_hex(x, y, radius, w=1, color=(255, 255, 255)):
+    return [color, [(x + radius * cos(tau * i / 6), y + radius * sin(tau * i / 6)) for i in range(6)], w]
 
 class HexUI:
     def __init__(self, hex, x, y, radius, hex_color=(255, 255, 255), text_color=(255, 255, 255)):
@@ -14,10 +14,12 @@ class HexUI:
         self.radius = radius
         self.hex_color = hex_color
         self.text_color = text_color
+        self.fill_color = (0, 0, 0)
     
     def draw(self, surface):
         # Draw the hexagon
-        pygame.draw.polygon(surface, *draw_hex(self.x, self.y, self.radius, self.hex_color))
+        pygame.draw.polygon(surface, *draw_hex(self.x, self.y, self.radius, 0, self.fill_color))
+        pygame.draw.polygon(surface, *draw_hex(self.x, self.y, self.radius, 1, self.hex_color))
         
         # Draw the text in the hexagon
         font = pygame.font.SysFont("Courier New", 30)
