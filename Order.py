29e82b5db2fdab49befea97794002df1d86e0083
Pacil142857 <austinmcgregor2@gmail.com>
2023-10-14 16:26:36 -0400

    Add method to change every variable at once

--- Order.py
+++ Order.py
@@ -1,5 +1,11 @@
-class Order:
+class Order():
     def __init__(self, hex, points, quantity_name, unit_name):
+        self.hex = hex
+        self.points = points
+        self.quantity_name = quantity_name
+        self.unit_name = unit_name
+        
+    def change(self, hex, points, quantity_name, unit_name):
         self.hex = hex
         self.points = points
         self.quantity_name = quantity_name
