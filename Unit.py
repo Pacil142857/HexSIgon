edd6a961b8c41784bcaa53809d7f137716e99ee6
Saebyul Hong <104213415+StarshineStudios@users.noreply.github.com>
2023-10-14 14:44:17 -0400

    added units and constants

---
+++ Unit.py
@@ -0,0 +1,6 @@
+class Unit:
+    def __init__(self, symbol, name, quantity):
+        self.symbol = self # Eg: s
+        self.name = name # Eg: Second
+        self.quantity = quantity # Eg: time
+        # self.description = description # Eg: "The second, symbol s, is the SI unit of time. It is defined by taking the fixed numerical value of the caesium frequency, ∆νCs, the unperturbed ground-state hyperfine transition frequency of the caesium 133 atom, to be 9192631770 when expressed in the unit Hz, which is equal to s−1."
