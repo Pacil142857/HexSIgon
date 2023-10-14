e0924a94f1a1d6fabaaa15ad5eba2d75c4149c37
Pacil142857 <austinmcgregor2@gmail.com>
2023-10-14 13:39:05 -0400

    Add a few vars

--- Order.py
+++ Order.py
@@ -1,8 +1,6 @@
 class Order:
-    def __init__(self, hex, quantity_name):
+    def __init__(self, hex, points, quantity_name, unit_name):
         self.hex = hex
+        self.points = points
         self.quantity_name = quantity_name
-    
-    
-    
-    
+        self.unit_name = unit_name
